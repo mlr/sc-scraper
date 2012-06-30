ca48765f6286ac8ff9b75acbbfc04541cbeeec7c
Ronnie Miller <ronniemiller@gmail.com>
2012-06-30 00:59:49 -0700

    Initial prototype

---
+++ scrape.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+require 'rubygems'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open("http://dubstep.net/"))
+doc.xpath('//div[@class="action_button_container"]/a').each do |node|
+  if node.text =~ /Download/
+    title    = node.parent.parent.xpath('.//h2/a').text
+    url      = node.attr('href')
+    filename = "#{title}.mp3"
+
+    unless File.exist?(filename)
+      `curl -L #{url} -o '#{filename}'`
+    else
+      puts "#{filename} already downloaded."
+    end
+  end
+end
+
