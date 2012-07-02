903bf8f9fa336d5f57a63bd5ede4a6279be05828
Ronnie Miller <ronniemiller@gmail.com>
2012-07-02 00:46:34 -0700

    Added simple pagination

--- scrape.rb
+++ scrape.rb
@@ -2,17 +2,23 @@ require 'open-uri'
 require 'rubygems'
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open("http://dubstep.net/"))
+page = ARGV[0] || 1
+
+doc = Nokogiri::HTML(open("http://dubstep.net/p/#{page}"))
 doc.xpath('//div[@class="action_button_container"]/a').each do |node|
   if node.text =~ /Download/
-    title    = node.parent.parent.xpath('.//h2/a').text
+    title    = node.parent.parent.xpath('h2/a').text
     url      = node.attr('href')
     filename = "#{title}.mp3"
 
     unless File.exist?(filename)
-      `curl -L #{url} -o '#{filename}'`
+      puts
+      puts "Downloading #{title}"
+      puts
+
+      `curl -L #{url} -o "#{filename}"`
     else
-      puts "#{filename} already downloaded."
+      puts "Already downlaoded #{filename}"
     end
   end
 end
