9d7436ba52e470d9e990b9d907dc9a7d887c9bd2
Silas Sao <silassao@gmail.com>
2012-08-20 09:21:29 -0700

    rewrote some stuff, added paging capabilities

--- scrape.rb
+++ scrape.rb
@@ -1,25 +1,51 @@
 require 'open-uri'
 require 'rubygems'
 require 'nokogiri'
+require 'mechanize'
 
-page = ARGV[0] || 1
+downloader = Mechanize.new
+downloader.pluggable_parser.default = Mechanize::Download
 
-doc = Nokogiri::HTML(open("http://dubstep.net/p/#{page}"))
-doc.xpath('//div[@class="action_button_container"]/a').each do |node|
-  if node.text =~ /Download/
-    title    = node.parent.parent.xpath('h2/a').text
-    url      = node.attr('href')
-    filename = "#{title}.mp3"
 
-    unless File.exist?(filename)
-      puts
-      puts "Downloading #{title}"
-      puts
+print "Enter Soundcloud username: "
+username = gets
+username = username.chomp
 
-      `curl -L #{url} -o "#{filename}"`
+print "Enter how many pages to scrape: "
+count = gets
+count = count.chomp
+
+urls = {}
+base = "http://soundcloud.com/#{username}"
+
+paged_urls = (1..count.to_i).collect do |num|
+  "#{base}/tracks?format=html&page=#{num}"
+end
+
+paged_urls.each do |page|
+  page = Nokogiri::HTML(open(page))
+  tunes = page.css('ul.tracks-list li.player')
+  tunes.each do |tune|
+    title = tune.css('div.info-header h3 a').text
+    link = tune.css('div.actionbar div.actions div.primary a.download')
+
+    unless link.empty?
+      url = "http://soundcloud.com" + link.attr('href')
     else
-      puts "Already downlaoded #{filename}"
+      next
     end
+    
+    urls[title] = url
   end
 end
 
+urls.each do |key,value|
+  filename = "#{key}.mp3"
+
+  unless File.exist?(filename)
+    puts "Downloading #{key}..."
+    downloader.get(value).save(filename)
+  else
+    puts "Skipped file (already exists)..."
+  end
+end
